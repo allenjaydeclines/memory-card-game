f0b911e4829ec6b7438ece4a46578414a2ac2ad9
Allen Jay Declines <allen.jay.declines@oracle.com>
2023-12-18 13:56:18 +0800

    set to default app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Box, Grid, Typography } from "@mui/material";
-import { TodoListContainer } from "./components/TodoListContainer";
 
 function App() {
   return (
@@ -10,13 +9,12 @@ function App() {
             variant="h4"
             align="center"
           >
-            Todo List
+            Memory Game
           </Typography>
         </Box>
       </Grid>
       <Grid item xs={1} md={2} lg={3}/>
       <Grid item xs={10} md={8} lg={6}>
-        <TodoListContainer/>
       </Grid>
       <Grid item xs={1} md={2} lg={3}/>
     </Grid>
